db362b93501800fceb2922b5b82a3bd20f77ec5b
Tori <mes.viktoria@mail.ru>
2022-08-19 22:35:11 +0500

    Является ли число чётным

---
+++ Task006/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 6
+//Является ли число чётным
+
+Console.WriteLine("Пожалуйста, введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.Write($"Число {number} является чётным!");
+}
+else
+{
+    Console.Write($"Число {number} является нечётным!");
+}
