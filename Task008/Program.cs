0d0b6d49deacdf11dd17b1c9a6c836a72402c307
Tori <mes.viktoria@mail.ru>
2022-08-19 23:26:48 +0500

    Все чётные числа от 1 до N

---
+++ Task008/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 9
+//Все чётные числа от 1 до N
+
+Console.WriteLine("Пожалуйста, введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int numberA = 1; numberA <= number; numberA++)
+    if (numberA % 2 == 0)
+    {
+        Console.Write(numberA + " ");
+    }
+    
