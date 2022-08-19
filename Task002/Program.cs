989c250b76c353450ddc09b1db4786cdf4558be2
Tori <mes.viktoria@mail.ru>
2022-08-19 21:41:10 +0500

    Большее и меньшее число

---
+++ Task002/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 2
+//Какое число большее, какое меньшее
+
+Console.WriteLine("Пожалуйста, введите первое число: ");
+int FirstNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста, введите второе число: ");
+int SecondNumber = int.Parse(Console.ReadLine());
+
+if (FirstNumber > SecondNumber) 
+{
+    Console.Write("Первое число больше: ");
+    Console.Write(FirstNumber);
+    Console.Write(", второе число меньше: ");
+    Console.Write(SecondNumber);
+}
+else
+{
+    Console.Write("Второе число больше: ");
+    Console.Write(SecondNumber);
+    Console.Write(", первое число меньше: ");
+    Console.Write(FirstNumber);
+}
